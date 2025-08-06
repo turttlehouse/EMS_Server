44926a062a66e36d6bd471f19e05e95fff436ca5
turttlehouse <builandrun464@gmail.com>
2025-08-06 08:12:30 +0545

    dotenve configuration with dotenv package

--- dist/app.js
+++ dist/app.js
@@ -4,8 +4,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 const app = (0, express_1.default)();
-const PORT = 5000;
+const PORT = Number(process.env.PORT);
 app.get('/', (req, res) => {
     res.send('server connected successfully!');
 });
--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import express from 'express';
 import { Application, Request, Response } from 'express';
-const app: Application = express();
-const PORT: number = 5000;
+import dotenv from 'dotenv';
 
+dotenv.config();
+
+const app: Application = express();
+const PORT: number = Number(process.env.PORT) || 5000;
 
 app.get('/', (req: Request, res: Response) => {
     res.send('server connected successfully!');
