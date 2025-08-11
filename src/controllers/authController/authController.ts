fd614d5aa7afc04dd5845cb71903c16732d904c3
turttlehouse <builandrun464@gmail.com>
2025-08-11 12:20:37 +0545

    feat(auth): add user management(get, delete, update role)

--- src/controllers/authController/authController.ts
+++ src/controllers/authController/authController.ts
@@ -50,6 +50,7 @@ class AuthController{
 
     }
 
+    // login user method
     public static async loginUser(req:AuthRequest,res:Response):Promise<void>{
         const { email,passwordHash} = req.body;
 
@@ -97,6 +98,108 @@ class AuthController{
             data : token
         })
 
+    }
+
+    // get all users
+    public static async getUsers(req:AuthRequest,res:Response,next:NextFunction):Promise<void>{
+       
+        const users = await User.findAll({
+            attributes: ['id', 'username', 'email', 'role']
+        });
+        
+        if(users.length > 0){
+            res.status(200).json({
+                message : 'users fetched successfully',
+                data : users
+            })
+            return;
+        }
+        else{
+            res.status(200).json({
+                message : 'users not found',
+                data : []
+            })
+        }
+        
+    }
+
+    // delete user
+    public static async deleteUser(req:AuthRequest,res:Response,next:NextFunction):Promise<void>{
+        
+        const id = req.params.id;
+
+        const user = await User.findOne({
+            where:{
+                id: id
+            }
+        })
+
+        if(!user){
+            res.status(400).json({
+                message : 'user not found'
+            })
+            return;
+        }
+
+        if(user.role === 'admin'){
+            res.status(400).json({
+                message : 'admin user cannot be deleted'
+            })
+        }
+
+        await User.destroy({
+            where : {
+                id : id
+            }
+        })
+
+        res.status(200).json({
+            message : 'user deleted successfully'
+        })
+    }
+
+    // update user role
+    public static async updateUserRole(req:AuthRequest,res:Response,next:NextFunction):Promise<void>{
+        
+        const id = req.params.id;
+        const {role} = req.body;
+
+        if(!role || !['admin','teacher','student'].includes(role)){
+            res.status(400).json({
+                message : 'please provide a valid role'
+            })
+            return;
+        }
+
+        const user = await User.findOne({
+            where:{
+                id: id
+            }
+        })
+
+        if(!user){
+            res.status(400).json({
+                message : 'user not found'
+            })
+            return;
+        }
+
+        if(user.role === 'admin'){
+            res.status(400).json({
+                message : 'admin user cannot be updated'
+            })
+            return;
+        }
+
+        await User.update({role: role},{
+            where : {
+                id : id
+            }
+        })
+
+        res.status(200).json({
+            message : 'user role updated successfully'
+        })
     }
 }
 
--- src/routes/authRoute/authRoute.ts
+++ src/routes/authRoute/authRoute.ts
@@ -1,6 +1,8 @@
 import express,{Router} from 'express'
 import errorHandler from '../../services/errorHandler';
 import AuthController from '../../controllers/authController/authController';
+import authMiddleware from '../../middleware/auth/authMiddleware';
+import { Role } from '../../middleware/auth/authTypes';
 
 const router : Router = express.Router();
 
@@ -10,6 +12,14 @@ router.route('/register')
 router.route('/login')
 .post(errorHandler(AuthController.loginUser))
 
+router.route('/users')
+.get(authMiddleware.isAuthenticated,authMiddleware.restrictTo(Role.Admin),errorHandler(AuthController.getUsers))
+
+router.route('/users/:id')
+.delete(authMiddleware.isAuthenticated,authMiddleware.restrictTo(Role.Admin),errorHandler(AuthController.deleteUser))
+.patch(authMiddleware.isAuthenticated,authMiddleware.restrictTo(Role.Admin),errorHandler(AuthController.updateUserRole))
+
+// router.route('/users/:id/role')
 
 export default router;
 
